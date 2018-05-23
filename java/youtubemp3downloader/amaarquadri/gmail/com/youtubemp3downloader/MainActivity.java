38266f4a737b1124b6c703e2b87e4adb3895d42a
AQUD <amaar.quadri@capco.com>
2018-05-23 16:12:15 -0400

    Initial commit

---
+++ java/youtubemp3downloader/amaarquadri/gmail/com/youtubemp3downloader/MainActivity.java
@@ -0,0 +1,13 @@
+package youtubemp3downloader.amaarquadri.gmail.com.youtubemp3downloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
